67474f0157e0f95fc1d9fdefb68a48ac632985e2
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-08-13 01:27:04 -0400

    Clean up code.

--- dwt.c
+++ dwt.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <ncurses.h>
 
 const char TASK_LIST_FILE[] = "tasks.txt";
 
-FILE * open_task_list() {
+FILE* open_task_list(char *mode_string) {
   FILE *task_list;
 
-  task_list = fopen(TASK_LIST_FILE, "a+");
+  task_list = fopen(TASK_LIST_FILE, mode_string);
 
   if(task_list == NULL) {
     fprintf(stderr, "Can't open task list: %s\n", TASK_LIST_FILE);
@@ -17,22 +18,70 @@ FILE * open_task_list() {
   return task_list;
 }
 
-void close_task_list(FILE * task_list) {
+int close_task_list(FILE *task_list) {
   fclose(task_list);
+
+  return 1;
+}
+
+int create_task(char *task, FILE *task_list) {
+  if((task_list = open_task_list("a"))) {
+    fputs(task, task_list);
+    fputc('\n', task_list);
+
+    close_task_list(task_list);
+
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int delete_task(char *task, FILE *task_list) {
+  return 1;
+}
+
+int read_tasks(char *tasks[], FILE *task_list) {
+  int i = 0;
+  char *task;
+  char buffer[100];
+
+  if((task_list = open_task_list("r"))) {
+    while(fgets(buffer, sizeof(buffer), task_list) != NULL) {
+      if((task = malloc(sizeof(char) * 100)) != NULL ) {
+        strcpy(task, buffer);
+        tasks[i] = task;
+        i++;
+      }
+    }
+
+    close_task_list(task_list);
+
+    return i;
+  } else {
+    return 0;
+  }
 }
 
 int main(int argc, char *argv[]) {
-  int option, cflag, lflag, iflag;
-  char task[100];
+  int i, option, task_list_size;
+  int cflag = 0, lflag = 0, dflag = 0, iflag = 0;
+  char *cvalue, *dvalue;
+  char *tasks[100];
   FILE *task_list;
 
-  while((option = getopt(argc, argv, "ilc:")) != -1) {
+  while((option = getopt(argc, argv, "ilc:d:")) != -1) {
     switch(option) {
     case 'i':
       iflag = 1;
       break;
     case 'c':
       cflag = 1;
+      cvalue = optarg;
+      break;
+    case 'd':
+      dflag = 1;
+      dvalue = optarg;
       break;
     case 'l':
       lflag = 1;
@@ -42,8 +91,6 @@ int main(int argc, char *argv[]) {
       exit(EXIT_FAILURE);
     }
   }
-
-  task_list = open_task_list();
 
   if(iflag) {
     initscr();
@@ -56,19 +103,30 @@ int main(int argc, char *argv[]) {
     getch();
     endwin();
   } else {
+    if(dflag) {
+      if(delete_task(dvalue, task_list) == 0) {
+        fprintf(stderr, "Could not delete task!");
+      }
+    }
+
     if(cflag) {
-      fputs(optarg, task_list);
-      fputc('\n', task_list);
+      if(create_task(cvalue, task_list) == 0) {
+        fprintf(stderr, "Could not create task!");
+      }
     }
 
     if(lflag) {
-      while(fgets(task, sizeof(task), task_list) != NULL) {
-        printf("%s", task);
+      printf("TODOs\n\n");
+
+      if((task_list_size = read_tasks(tasks, task_list))) {
+        for(i = 0; i < task_list_size; i++) {
+          printf("%s", tasks[i]);
+        }
+      } else {
+        printf("None!\n");
       }
     }
   }
-
-  close_task_list(task_list);
 
   return 0;
 }
